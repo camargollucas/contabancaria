aebcd8f86d94ce842fbd4b9ae743f56d3010cf35
Lucas <camargolimalucas@gmail.com>
2023-12-20 12:25:24 -0300

    criação conta.controller

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,6 +2,7 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.controller.ContaController;
 import conta.model.Conta;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
@@ -9,14 +10,39 @@ import conta.util.Cores;
 
 public class Menu {
 
-	public static Scanner leia = new Scanner(System.in);
-	
 	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
 
-		int opcao;
+		ContaController contas = new ContaController();
+
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
+		String n;
+
+		System.out.println("Deseja criar contas teste?: S/N");
+		n = leia.next();
 	
+		if(n.contains("S")){ 
+		
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "João", 1000f, 100f);
+		contas.cadastrar(cc1);
+		
+		//ContaCorrente cc2 = new ContaCorrente(2, 123, 1, "Maria", 2000f, 100f);
+		//contas.cadastrar(cc2);
+
+		ContaPoupanca cp1 = new ContaPoupanca(3, 125, 2, "José", 4000f, 12);
+		contas.cadastrar(cp1);
+		
+	//	ContaPoupanca cp2 = new ContaPoupanca(4, 125, 2, "Helena", 8000f, 15);
+	//	contas.cadastrar(cp2);
+		
+		contas.listarTodas();}
+		
+
 		while (true) {
-			
+
 			System.out.println(Cores.TEXT_WHITE_BOLD + Cores.ANSI_BLACK_BACKGROUND);
 			System.out.println("----------------------------------------------------\r\n"
 					+ "                                                    \r\n"
@@ -37,55 +63,90 @@ public class Menu {
 					+ "----------------------------------------------------\r\n"
 					+ "                                                    \r\n"
 					+ "Entre com a opção desejada:                         ");
-			
+
 			opcao = leia.nextInt();
-			
-			if(opcao == 9) {
+
+			if (opcao == 9) {
 				System.out.println("\nBanco Colchão: Seu conforto financeiro é a nossa prioridade.");
-                  		sobre();
+				sobre();
 				leia.close();
 				System.exit(0);
 			}
-			
-			switch(opcao) {
+
+			switch (opcao) {
 			case 1:
-				System.out.println("\n Criar conta");
-				
-                 break;
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\n Criar conta");
+
+				System.out.println("\n Digite o número da agência: ");
+				agencia = leia.nextInt();
+				System.out.println("\n Digite o nome do titular: ");
+				leia.skip("\\R?");
+				titular = leia.nextLine();
+
+				do {
+					System.out.println("Digite o tipo da conta: (1-Corrente ou 2-Poupança): ");
+					tipo = leia.nextInt();
+				} while (tipo < 1 && tipo > 2);
+
+				System.out.println("Digite o saldo da conta (R$): ");
+				saldo = leia.nextFloat();
+
+				switch (tipo) {
+				case 1 -> {
+					System.out.println("Digite o limite de crédito (R$): ");
+					limite = leia.nextFloat();
+					contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+				}
+
+				case 2 -> {
+					System.out.println("Digite o dia do aniversário da conta: ");
+					aniversario = leia.nextInt();
+					contas.cadastrar(
+							new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+				}
+				}
+
+				break;
 			case 2:
 				System.out.println("\n Listar todas as contas");
-				
-                 break;
+				contas.listarTodas();
+
+				break;
 			case 3:
 				System.out.println("\n Buscar conta por número");
-				
+
 				break;
 			case 4:
 				System.out.println("\n Atualizar dados da conta");
-				
-                 break;
+
+				break;
 			case 5:
 				System.out.println("\n Encerrar conta");
-				
-                 break;
+
+				break;
 			case 6:
 				System.out.println("\n Saques");
-				
+
 				break;
-             case 7:
+			case 7:
 				System.out.println("\n Depósitos");
-				
+
 				break;
-             case 8:
+			case 8:
 				System.out.println("\n Transferências");
-				
+
 				break;
 			default:
 				System.out.println("\nOpção inválida");
-                 break;
+				break;
 			}
-        }
+		}
 	}
+
+	private static void keyPress() {
+
+	}
+
 	public static void sobre() {
 		System.out.println("------------------------------------------------------------");
 		System.out.println("Projeto Desenvolvido por: Lucas Camargo                     ");
